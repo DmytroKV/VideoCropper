e35abf1124c1f77a865243ffdab050622e2176c6
DmytroKV <dmytrokocyuba@gmail.com>
2021-01-12 12:27:07 +0200

    Second try of doing task

--- src/config/index.js
+++ src/config/index.js
@@ -20,18 +20,26 @@ app.use(
   );
   */
   app.post("/crop", (req, res) => {
-    var path = 'C:/drop.avi'
+    var path = '/home/ooyashi/Downloads/small.mp4'
     //var file = fs.readFileSync(path, 'binary');
 
-    var process = new ffmpeg(path);
-    process.then(function (video) {
-            // Video metadata
-            console.log(video.metadata);
-            // FFmpeg configuration
-            console.log(video.info_configuration);
-            //video.setVideoAspectRatio('16:9');
-        })
-
+    try {
+      //Problem:
+      //By shell it displays metadata, but node.js for some reason cannot!
+      var process = new ffmpeg(path);
+      process.then(function (video) {
+        // Video metadata
+        console.log(video.metadata);
+        // FFmpeg configuration
+        console.log(video.info_configuration);
+      }, function (err) {
+        console.log('Error: ' + err);
+      });
+    } catch (e) {
+      console.log(e.code);
+      console.log(e.msg);
+    }
+    
 
   });
 app.get("/", (req, res) => {
@@ -42,3 +50,6 @@ app.listen(5000, () => {
 })
         //const file = `${__dirname}`;
         //res.download(file); // Set disposition and send it.
+
+
+//ffmpeg -i /home/ooyashi/Downloads/small.mp4 -f ffmetadata - checking metadata by vscode terminal
