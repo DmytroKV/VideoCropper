fac6e5dc4407d96e89d6f74bf50dcdcd61d0b1e2
DmytroKV <dmytrokocyuba@gmail.com>
2021-01-12 17:08:57 +0200

    Additionals to project

--- src/config/index.js
+++ src/config/index.js
@@ -7,6 +7,13 @@ const Child_process = require("child_process");
 var fs = require('fs');
 var ffmpeg = require('ffmpeg');
 
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
+app.use("/tmp", express.static(__dirname + '/tmp'));
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -15,11 +22,12 @@ app.use(bodyParser.json());
 
      //ffmpeg -i /home/ooyashi/Downloads/small.mp4 -f ffmetadata - checking metadata by vscode terminal
   app.post("/video", (req, res) => {
-    const uploadingPath = __dirname;
+    res.send('This is /video API');
 
     Child_process.execSync(
       'ffmpeg -i /home/ooyashi/Documents/VideoCropperClone/VideoCropper/1280.avi -s 1280x720 -vf crop=720:720,setdar=1:1,setsar=1:1 1280_1.avi'
     )
+    
   });
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
@@ -27,3 +35,11 @@ app.get("/", (req, res) => {
 app.listen(5000, () => {
     console.log("App is listening on Port 5000")
 })
+/*
+Child_process.execSync(
+  `ffmpeg -i ${
+    video
+  } -s ${width}x${height} -vf crop=${size}:${size},setdar=1:1,setsar=1:1 ${
+    uploadingPath
+  }`
+  */
