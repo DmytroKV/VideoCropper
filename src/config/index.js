23e7b68718cd2911337abcd1580425c2f2654e6f
DmytroKV <dmytrokocyuba@gmail.com>
2021-01-12 15:47:45 +0200

    Done the first task

--- src/config/index.js
+++ src/config/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser');
 const fileUpload = require("express-fileupload");
+const Child_process = require("child_process");
 
 var fs = require('fs');
 var ffmpeg = require('ffmpeg');
@@ -11,51 +12,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
 app.use(bodyParser.json());
-/*
-app.use(
-    fileUpload({
-      useTempFiles: true,
-      tempFileDir: "/tmp/",
-    })
-  );
-  */
-  app.post("/video", (req, res) => {
 
-    //var spawn = require('child_process').spawn;
-    //var cmd = '/snap/bin/ffmpeg';
-    var path = '/home/ooyashi/Downloads/small.mp4'
-    /*
-    var args = [
-      '-i', '/home/ooyashi/Downloads/small.mp4',
-      '-f', 'ffmetadata'
-    ];
-    */
-    //var process = spawn(cmd, args)
-    //var file = fs.readFileSync(path, 'binary');
-    //ffmpeg -i /home/ooyashi/Downloads/small.mp4 -f ffmetadata - checking metadata by vscode terminal
-    
-      const {spawn} = require('child_process');
-      
-      const child = spawn('ffmpeg', [
-        '-i',
-        ' /home/ooyashi/Downloads/samplemkv.mkv',
-        ' -f',
-        ' ffmetadata'
-      ])
-      
-     
-      child.stdout.on('data', function(data) {
-        console.log(data);
-    });
-    
-    child.stderr.setEncoding("utf8")
-    child.stderr.on('data', function(data) {
-        console.log(data);
-    });
-  
-    child.on('close', function() {
-    console.log('finished');
-    });
+     //ffmpeg -i /home/ooyashi/Downloads/small.mp4 -f ffmetadata - checking metadata by vscode terminal
+  app.post("/video", (req, res) => {
+    const uploadingPath = __dirname;
+
+    Child_process.execSync(
+      'ffmpeg -i /home/ooyashi/Documents/VideoCropperClone/VideoCropper/1280.avi -s 1280x720 -vf crop=720:720,setdar=1:1,setsar=1:1 1280_1.avi'
+    )
   });
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
@@ -63,8 +27,3 @@ app.get("/", (req, res) => {
 app.listen(5000, () => {
     console.log("App is listening on Port 5000")
 })
-        //const file = `${__dirname}`;
-        //res.download(file); // Set disposition and send it.
-
-
-//ffmpeg -i /home/ooyashi/Downloads/small.mp4 -f ffmetadata - checking metadata by vscode terminal
