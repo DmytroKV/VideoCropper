27076984f9965c79af7eec83b6c4bb018644361b
DmytroKV <dmytrokocyuba@gmail.com>
2021-01-13 14:40:40 +0200

    Fixed and completed task

--- src/config/index.js
+++ src/config/index.js
@@ -7,6 +7,9 @@ const { v4: uuidv4 } = require('uuid');
 
 var fs = require('fs');
 var ffmpeg = require('ffmpeg');
+var ffmpeg_fluent = require('fluent-ffmpeg');
+
+ffmpeg_fluent.setFfprobePath('/snap/bin/ffmpeg.ffprobe');
 
 app.use(
   fileUpload({
@@ -22,13 +25,46 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
   app.post("/video", (req, res) => {
-    var filename = __dirname + '/../tmp/' + uuidv4() + '.avi';
+    //storing file data
+    let file = req.files.file;
+    //getting temporary path file for processing
+    let pathFile = file.tempFilePath;
+    //getting file name for crop command
+    let fileUploadName = file.name; 
 
+    var width; 
+    var height;
+
+    //getting width and height from ffmpeg command
+    width = Child_process.execSync(
+      `ffprobe -v error -show_entries stream=width -of csv=p=0:s=x ${
+        pathFile}`
+    )
+    height = Child_process.execSync(
+      `ffprobe -v error -show_entries stream=height -of csv=p=0:s=x ${
+        pathFile}`
+    )
+    //parsing integers from result strings (I know, bad approach, 
+    //but no ffmpeg libraries have given me such info :/)
+
+    let realWidth = parseInt(width, 10);
+    let realHeight = parseInt(height, 10);
+
+    //file info
+    console.log('File ' + fileUploadName + ' has resolution of ' + realWidth + 'x' + realHeight);
+    
+    //unique name for processed video
+    var filename = __dirname + '/../tmp/' + uuidv4() + '.avi';
+    
     Child_process.execSync(
-      'ffmpeg -i /home/ooyashi/Documents/VideoCropperClone/VideoCropper/1280.avi -s 1280x720 -vf crop=720:720,setdar=1:1,setsar=1:1 ' + filename
+      `ffmpeg -i ${
+        fileUploadName
+      } -s ${realWidth}:${realHeight} -vf crop=${realHeight}:${realHeight},setdar=1:1,setsar=1:1 ` + filename
     )
     
-    res.json({videoURL : "/tmp/" + filename});
+    //video url for download
+    res.json({videoURL : filename});
+    
   });
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
@@ -36,11 +72,7 @@ app.get("/", (req, res) => {
 app.listen(5000, () => {
     console.log("App is listening on Port 5000")
 })
-/*
-Child_process.execSync(
-  `ffmpeg -i ${
-    video
-  } -s ${width}x${height} -vf crop=${size}:${size},setdar=1:1,setsar=1:1 ${
-    uploadingPath
-  }`
-  */
+//WIDTH
+//ffprobe -v error -show_entries stream=width -of csv=p=0:s=x /home/ooyashi/Documents/VideoCropperClone/VideoCropper/1280.avi
+//HEIGHT
+//ffprobe -v error -show_entries stream=height -of csv=p=0:s=x /home/ooyashi/Documents/VideoCropperClone/VideoCropper/1280.avi
